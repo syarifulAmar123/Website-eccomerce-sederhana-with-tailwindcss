60d1d41d0522098f70d69098282dc3e59cd2865b
Syariful Amar <kingnazaer221@gmail.com>
2025-04-25 19:59:12 +0700

    commit awal

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        tembelek: "#1f1f1f",
+      },
+      animation: {
+        "spin-slow": "spin 5s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
